666d39eaf1ade207b83f06ce9a5e0b63936f3b60
Madhav <madhav.khanna@tothenew.com>
2016-07-23 23:48:57 +0530

    Basic spring boot app is set up

---
+++ src/main/java/com/intelligrape/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.intelligrape.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+
+@Controller("loginController")
+@RequestMapping("/")
+@Scope("session")
+public class LoginController {
+
+
+    @RequestMapping(value = {"/"})
+    @ResponseBody
+    public String homePage() {
+//        response body explanation
+//        http://stackoverflow.com/questions/7672858/return-only-string-message-from-spring-mvc-3-controller
+        return "Home Page";
+    }
+
+}
